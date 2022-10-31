82be03303504807ddd52881345128d0d372b34e6
lonelytechguy <115178655+lonelytechguy@users.noreply.github.com>
2022-10-31 15:26:53 +0300

    Create lists.py

---
+++ Python Lists/lists.py
@@ -0,0 +1,11 @@
+# Create a Python list called languages
+
+languages = ["Java", "Python", "PHP", "JavaScript" ]
+
+# Access Java using index 0
+
+print(languages[0])
+
+# Access PHP using index 2
+
+print(languages[2])
