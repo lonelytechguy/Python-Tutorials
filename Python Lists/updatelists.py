d33752b0b8fc3122fffefe04fde31a26e5ae418c
lonelytechguy <115178655+lonelytechguy@users.noreply.github.com>
2022-11-10 12:38:27 +0300

    Create updatelists.py

---
+++ Python Lists/updatelists.py
@@ -0,0 +1,15 @@
+# Create original list
+languages = ["Java", "Python", "Language3"]
+
+# Print original list
+print("This is the original list: ", languages)
+
+# Update the item of index 2
+languages[2] = "JavaScript"
+
+# Print updated list
+print("This is the updated list", languages)
+
+# Access updated list items using a loop
+for i in languages:
+	print(i)
